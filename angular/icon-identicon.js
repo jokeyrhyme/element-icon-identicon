3c7d87036768e1b23662c52bb40240cf80f163a0
Ron Waldon <jokeyrhyme@gmail.com>
2014-07-13 09:38:20 +1000

    rename and UMD-wrap identicon-canvas library

--- angular/icon-identicon.js
+++ angular/icon-identicon.js
@@ -5,17 +5,17 @@
   'use strict';
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['angular'], factory);
+    define(['angular', '../lib/identicon-canvas'], factory);
   } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    module.exports = factory(require('angular'));
+    module.exports = factory(require('angular'), require('../lib/identicon-canvas'));
   } else {
     // Browser globals (root is window)
-    factory(root.angular);
+    factory(root.angular, root.identiconCanvas);
   }
-}(this, function (angular) {
+}(this, function (angular, identiconCanvas) {
   'use strict';
 
   return {};
